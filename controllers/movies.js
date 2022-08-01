2e1f7893236974b1caaf5d968d61e7daf73182a6
Aleksandr Demidenko <sashatro@mail.ru>
2022-08-01 18:18:36 +0300

    Пользователю показывать только его фильмы

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,8 +5,11 @@ const ForbiddenError = require('../errors/forbidden-err');
 const NotFoundError = require('../errors/not-found-err');
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
-    .then((movies) => res.send(movies))
+  const owner = req.user._id;
+  Movie.find({ owner })
+    .then((movies) => {
+      res.status(200).send(movies);
+    })
     .catch(next);
 };
 
